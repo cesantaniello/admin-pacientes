8ee794c9c30e14ab0779d04d4ef23a698e6825ae
Carlos Santaniello <carlos.e.santaniello@gmail.com>
2023-06-02 11:05:21 +0200

    Iterate paciente array

--- src/components/ListadoPacientes.jsx
+++ src/components/ListadoPacientes.jsx
@@ -1,6 +1,7 @@
 import Paciente from "./Paciente"
 
-const ListadoPacientes = () => {
+const ListadoPacientes = ({pacientes}) => {
+
     return (
       <div className="md:w-1/2 lg:w-3/5 md:h-screen overflow-y-scroll">
           <h2 className="font-black text-3xl text-center">Listado de Pacientes</h2>
@@ -8,7 +9,11 @@ const ListadoPacientes = () => {
             Administra tus {''}
             <span className="text-indigo-600 font-bold">pacientes y citas</span>
           </p>
-          <Paciente />
+
+          { 
+            pacientes.map(paciente =>  <Paciente paciente={paciente}/>)
+          }
+          
       </div>
     )
   }
