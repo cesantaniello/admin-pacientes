9cb1903d2d2cfefde04addf3e14c2b9ba525f17f
Carlos Santaniello <carlos.e.santaniello@gmail.com>
2023-06-02 12:58:07 +0200

    Show message in Listado de pacientes

--- src/components/ListadoPacientes.jsx
+++ src/components/ListadoPacientes.jsx
@@ -2,9 +2,14 @@ import Paciente from "./Paciente"
 
 const ListadoPacientes = ({pacientes}) => {
 
-    return (
-      <div className="md:w-1/2 lg:w-3/5 md:h-screen overflow-y-scroll">
-          <h2 className="font-black text-3xl text-center">Listado de Pacientes</h2>
+  console.log(pacientes)
+
+  
+  return (
+    <div className="md:w-1/2 lg:w-3/5 md:h-screen overflow-y-scroll">
+        {pacientes && pacientes.length ? (
+          <>          
+        <h2 className="font-black text-3xl text-center">Listado de Pacientes</h2>
           <p className="text-xl mt-5 mb-10 text-center">
             Administra tus {''}
             <span className="text-indigo-600 font-bold">pacientes y citas</span>
@@ -18,6 +23,17 @@ const ListadoPacientes = ({pacientes}) => {
               />
             ))
           }
+          </>
+        ) : (
+          <>
+            <h2 className="font-black text-3xl text-center">No hay pacientes</h2>
+            <p className="text-xl mt-5 mb-10 text-center">
+              Comienza agregando pacientes {''}
+              <span className="text-indigo-600 font-bold">y aparecerán en este lugar</span>
+            </p>
+          </>
+        )}
+
           
       </div>
     )
