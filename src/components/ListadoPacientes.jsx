b55dc449182fe3506d7df0e72ea8fc25c2475286
Carlos Santaniello <carlos.e.santaniello@gmail.com>
2023-06-02 15:27:18 +0200

    Setting state to paciente

--- src/components/ListadoPacientes.jsx
+++ src/components/ListadoPacientes.jsx
@@ -1,11 +1,6 @@
-import { useEffect } from "react";
 import Paciente from "./Paciente"
 
 const ListadoPacientes = ({pacientes, setPaciente}) => {
-
-  useEffect(() => {
-    console.log("nuevo paciente");
-  }, [pacientes])
   
   return (
     <div className="md:w-1/2 lg:w-3/5 md:h-screen overflow-y-scroll">
